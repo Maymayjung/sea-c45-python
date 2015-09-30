21748dc8db4e56c246fd15c46c33204ebd08858d
Maymayjung <th.sudarat@gmail.com>
2015-09-29 17:51:52 -0700

    Add initial homework file

--- hw/hw07/series.py
+++ hw/hw07/series.py
@@ -1,5 +1,12 @@
 def fibonacci(n):
 
+    """Example:
+    def fib(n):
+    if n==1 or n==2:
+    return 1
+    return fib(n-1)+fib(n-2)
+    print fib(5)
+    """
     if (n == 0):
         return 0
 
