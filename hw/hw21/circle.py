040015dc1e63d8fe3796ba60a4e3b97d2fee8fc2
Maymayjung <th.sudarat@gmail.com>
2015-10-07 16:25:12 -0700

    get start on circle.py

--- hw/hw21/circle.py
+++ hw/hw21/circle.py
@@ -4,26 +4,37 @@
 fill this in so it will pass all the tests.
 """
 import math
+import trigrams
+
+
+def test_open_file():
+    trigrams.test_open_file()
+
+
+def test_read_lines():
+    lines = trigrams.open_file()
+    assert (len(lines) > 0)
 
 
 class Circle(object):
 
     def _init_(self, radius):
-    """
-    Initialize a circle with a given radius.
-    """
+        """
+        Initialize a circle with a given radius.
+        """
     self.radius = radius
-    self.diameter = 2*radius
-    self.area = math.pi*radius**2
+    self.diameter = 2 * radius
+    self.area = math.pi * radius ** 2
 
     def _get_d(self):
         return self._diameter
+
     def _set_d(self, value):
-        self._diameter =value
-        self.radius = value/2
-        self.area = math.pi*self.radius**2
+        self._diameter = value
+        self.radius = value / 2
+        self.area = math.pi * self.radius ** 2
+
     def _del_d(self):
         del self._diameter
     diameter = property(_get_d, _set_d, _del_d)
     doc = "Largest distance between any two points on circle."
-
