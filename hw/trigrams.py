53491445a9375ca192b7542808a3f069ba8406b3
Maymayjung <th.sudarat@gmail.com>
2015-10-06 17:10:43 -0700

    First submit for trigrams

--- hw/trigrams.py
+++ hw/trigrams.py
@@ -1,3 +1,10 @@
+def get_random_new_key():
+    candidates = []
+    for n in key_list:
+        if (n[0] == new_text[-1]):
+            candidates.append(n)
+    new_text.append(candidates[random.randrange(0, len(candidates))][1])
+
 import random
 book = open('sherlock.txt', 'r')
 text_dict = {}
@@ -13,3 +20,14 @@ for i in range(2, len(text_list)):
 key_list = list(text_dict.keys())
 new_text = list(random.choice(key_list))
 new_text[0] = new_text[0].capitalize()
+
+for i in range(300):
+    if ((new_text[-2], new_text[-1]) in text_dict):
+        new_text.append(random.choice(text_dict[new_text[-2], new_text[-1]]))
+    else:
+        get_random_new_key()
+
+if (new_text[-1][-1] != "."):
+    new_text[-1] = new_text[-1] + "."
+
+print(' '.join(new_text))
