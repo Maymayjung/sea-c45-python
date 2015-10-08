3c10feb11b41650896c807108873df4e85bcc7a8
Maymayjung <th.sudarat@gmail.com>
2015-10-07 17:24:15 -0700

    add more code to circle

--- hw/hw21/circle.py
+++ hw/hw21/circle.py
@@ -37,4 +37,28 @@ class Circle(object):
     def _del_d(self):
         del self._diameter
     diameter = property(_get_d, _set_d, _del_d)
-    doc = "Largest distance between any two points on circle."
+
+    def __str__(self):
+        return "Circle with radius: %.6f" % (self.radius)
+
+    def __repr__(self):
+        return "Circle(%d)" % (self.radius)
+
+    def __add__(self, other):
+        value = self.radius.__add__(other.radius)
+        return Circle(value)
+
+    def __eq__(self, other):
+        return self.radius == other.radius
+
+    def __mul__(self, other):
+        value = self.radius.__mul__(other)
+        return Circle(value)
+
+    def __cmp__(self, other):
+        if self.radius > other.radius:
+            return 1
+        elif self.radius < other.radius:
+            return -1
+        else:
+            return 0
